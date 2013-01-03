2cc36a7cdf3e9dc47734e01537c195551e75d879
Ken <kkoch986@gmail.com>
2013-01-03 16:26:51 -0500

    generalized websearch and created quick search, also added mac key bindings

--- README.md
+++ README.md
@@ -3,9 +3,13 @@ SublimeWebSearch
 
 A sublime text 2 plugin to allow quick jumping to a web search. When the command is executed (note: a buffer must be open for the command to work), a text field will appear at the bottom of the screen. Enter something in there and hit enter and POW! web search.
 
+## Installation
+
+To install simply link or copy the WebSearch folder in the Packages directory (Sublime Text 2/Packages).
+
 ## Usage
 
-The default key binding for the search event is ctrl+k, you can change this in the *.sublime-keymap files included with this project. 
+The default key binding for the search event is ctrl+k (cmd+k on mac), you can change this in the *.sublime-keymap files included with this project. 
 
 ## Changing the search engine
 
--- WebSearch.py
+++ WebSearch.py
@@ -1,24 +1,100 @@
+'''
+' by Ken Koch 2013
+' Sublime Web Search Plugin
+'''
 import sublime, sublime_plugin
 import webbrowser
 import urllib
 
-class WebSearchCommand(sublime_plugin.TextCommand):
+'''
+	The Quick Web Search Command ("quick_web_search") takes the first search engine in the settings list
+	and opens an input for a query. Upon entering the query, a browser window is opened up displaying
+	search results for the given query.
+'''
+class QuickWebSearchCommand(sublime_plugin.TextCommand):
+	# this is a fallback url in case there isnt any found
 	defaultUrl = "http://google.com/search?"
+	defaultVar = "q"
+	defaultName = "Google"
 
 	def run(self, edit):
 		settings = sublime.load_settings("WebSearch.sublime-settings")
-		self.search = settings.get("com.kenkoch.websearch.search_engine", self.defaultUrl)
+		search_engines = settings.get("com.kenkoch.websearch.search_engines")
+		if(search_engines is None):
+			self._search = defaultUrl
+			self._var = defaultVar
+			self._name = defaultName
+		else:
+			index = settings.get("com.kenkoch.websearch.default_search_engine",0);
+			self._search = search_engines[index]["url"];
+			self._var = search_engines[index]["var"];
+			self._name = search_engines[index]["name"];
 
-		self.edit = edit
+
+		self._edit = edit
 		initial = ""
 		for r in self.view.sel(): 
 			if not r.empty():
 				initial = self.view.substr(r)	
 				break
 
-		self.view.window().show_input_panel("Search", initial, self.on_done, None, None)	
+		self.view.window().show_input_panel(self._name + " Search", initial, self.on_done, None, None)	
 
 
 	def on_done(self, string):
-		url = str(''.join((self.search, urllib.urlencode({"q":string}))))
+		url = str(''.join((self._search, urllib.urlencode({self._var:string}))))
 		webbrowser.open(url,new=2)
+
+'''
+	The Web Search Command ("web_search") opens a choice dialog of search engines defined in the WebSearch.sublime-settings file.
+	Once you choose one, it gives you a dialog to enter your query into. Upon entering that info, a browser window is opened
+	displaying search results on the given search engine.
+'''
+class WebSearchCommand(sublime_plugin.TextCommand):
+
+	def run(self, edit):
+		# load the settings and the list of search engines
+		self._edit = edit
+		settings = sublime.load_settings("WebSearch.sublime-settings")
+		self._search_engines = settings.get("com.kenkoch.websearch.search_engines")
+
+		# ensure there are engines to choose from
+		if(self._search_engines is None):
+			sublime.message_dialog("No Engines Loaded.")
+			return
+
+		# show a dialog to select from those choices
+		self.view.window().show_quick_panel([ [s["name"], s["desc"] ] for s in self._search_engines], self.on_choice)
+
+	# After a choice has been made:
+	# set the choice and show the input dialog
+	def on_choice(self, index):	
+		# ensure that a selection was in fact made
+		if(index == -1): 
+			return 
+
+		# load the info about that engine
+		self._search     = self._search_engines[index]["url"];
+		self._var        = self._search_engines[index]["var"];
+		self._name       = self._search_engines[index]["name"];
+
+		# grab the selected text
+		initial = ""
+		for r in self.view.sel(): 
+			if not r.empty():
+				initial = str(self.view.substr(r))	
+				break
+
+		# open the niput dialog
+		self.view.window().show_input_panel(self._name + " Search", initial, self.on_done, None, None)	
+
+	# After text has been entered
+	# Open the browser window.
+	def on_done(self, string):
+		# build the url
+		url = str(''.join((self._search, urllib.urlencode({self._var:string}))))
+
+		# open the browser
+		webbrowser.open(url,new=2)
+
+
